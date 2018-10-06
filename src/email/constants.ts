1aa2deb998072b0931656fba6d7f95dd71a9948b
Lukas Marx <marxlukas@googlemail.com>
2018-10-06 14:41:38 +0200

    spelling fix

--- src/email/constants.ts
+++ src/email/constants.ts
@@ -1,5 +1,5 @@
 export const SmtpUserKey = 'SMTP_USER';
-export const SmtpPasswordKey = 'SMTP_Password';
+export const SmtpPasswordKey = 'SMTP_PASSWORD';
 export const SmtpHost = 'email-smtp.eu-west-1.amazonaws.com';
 
 export const EmailTemplateToken = 'EmailTemplateToken';
